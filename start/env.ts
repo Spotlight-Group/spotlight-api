820a5f6554d2e6b37a62ff5163d4b898c08beb8b
NayanMallet <nayanmallet@gmail.com>
2026-01-22 10:55:33 +0100

    📦 feat(database): enhance database configuration for future test support

--- start/env.ts
+++ start/env.ts
@@ -23,11 +23,12 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring database connection
   |----------------------------------------------------------
   */
-  DB_HOST: Env.schema.string(),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
+  DB_CONNECTION: Env.schema.enum.optional(['mysql', 'sqlite'] as const),
+  DB_HOST: Env.schema.string.optional(),
+  DB_PORT: Env.schema.number.optional(),
+  DB_USER: Env.schema.string.optional(),
   DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string(),
+  DB_DATABASE: Env.schema.string.optional(),
 
   /*
   |----------------------------------------------------------
